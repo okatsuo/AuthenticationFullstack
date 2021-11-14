e22ebf7979834a15473acc78c2f57d6d46e05d71
okatsuo <rafael.goncanna@gmail.com>
2021-11-14 16:49:28 -0300

    feat: create user

--- backend/src/main/server.ts
+++ backend/src/main/server.ts
@@ -1,9 +1,12 @@
-import { prisma } from 'service/prisma'
+import { PrismaClient } from '.prisma/client'
+
+const prisma = new PrismaClient()
 
 void (async function Bootstrap () {
-  const newUser = prisma.user.create({
+  const newUser = await prisma.user.create({
     data: {
       name: 'Rafael',
+      email: 'rafael@mail.com',
       active: true,
       password: '12345'
     }
